0d6ea825b8cfe0840203d0d32b1cbc713aac92ec
seunghyun <inyo0506@gmail.com>
2023-11-30 18:06:10 +0900

    회원가입 수정중

--- js/register.js
+++ js/register.js
@@ -1,12 +1,45 @@
-const name = document.querySelector('.name input')
-const email = document.querySelector('.email input')
+const userName = document.querySelector('.name input')
+const userEmail = document.querySelector('.email input')
 const userId = document.querySelector('.userId input')
-const password = document.querySelector('.userPw input')
+const userPw = document.querySelector('.userPw input')
 
 
 const registerButton = document.querySelector('.register-btn')
 console.log(registerButton)
 async function getUserData(){
+    const regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    if(userName.value === ''){
+        alert('이름을 입력해주세요')
+        return
+    } 
+    else if (userName.value.length < 2){
+        alert('이름은 2글자 이상 입력해주세요')
+        return
+    }
+    else if(userEmail.value === ''){
+        alert('이메일을 입력해주세요')
+        return
+    } 
+    else if (userEmail.value.match(regExp) === null){
+        alert('이메일 형식이 올바르지 않습니다.')
+        return
+    } 
+    else if(userId.value === ''){
+       alert('아이디를 입력해주세요')
+       return
+    }  
+    else if(userId.value.length < 6 || userId.value.length > 12){
+        alert('아이디는 6자리 이상 12자리 미만으로 설정해주세요')
+        return
+     } 
+    else if(userPw.value === ''){
+       alert('비밀번호를 입력해주세요')
+       return
+    } else if (userPw.value.length < 6 || userPw.value.length > 12){
+        alert('비밀번호는 6자리 이상 12자리 미만으로 설정해주세요')
+        return
+    }  
+    else {
     try{
     const data = await fetch('https://port-0-bible-server-32updzt2alphmfpdy.sel5.cloudtype.app/api/users/register', 
     {
@@ -15,16 +48,17 @@ async function getUserData(){
             "Content-Type": "application/json"
         },
         body:JSON.stringify({
-            name: name.value,
-            email: email.value,
+            name: userName.value,
+            email: userEmail.value,
             userId: userId.value,
-            password: password.value
+            password: userPw.value
         })
     })
-    const userData = await data.json()
-    console.log(userData)
-}catch(error){
-    console.log(error)
+    const userData = await data.json()    
+    }
+catch(error){
+    console.log('회원가입 실패 :', error)
+}
 }
 }
 
