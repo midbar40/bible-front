871ed5318ae5db364f39c23b7119058616fa0a29
seunghyun <inyo0506@gmail.com>
2023-12-01 16:11:10 +0900

    필사로딩화면 추가

--- js/login.js
+++ js/login.js
@@ -19,7 +19,6 @@ async function getUserData(){
     try{
     const data = await fetch('https://port-0-bible-server-32updzt2alphmfpdy.sel5.cloudtype.app/api/users/login', {
     // const data = await fetch('http://127.0.0.1:3300/api/users/login', {
-
         method: 'POST',
         credentials: 'include', // 브라우저 쿠키탭에 토큰이 저장되기 위해서는 credentials: 'include' 옵션을 추가해줘야 한다.
         headers: {
@@ -38,8 +37,7 @@ async function getUserData(){
     }
     else if(userData.code === 200){
         alert('로그인이 완료되었습니다, 메인 페이지로 이동합니다.')
-        getCookie()
-        console.log('쿠키', document.cookie)
+        checkIsLogined()
         window.location.href = 'https://midbar40.github.io/bible-front/index.html'
     }
 }catch(error){
@@ -47,10 +45,19 @@ async function getUserData(){
 }
 }
 
-function getCookie() {
-    const cookie = document.cookie
-    const allCookie = cookie.split('; ')
-    console.log('쿠키', allCookie)
+
+const checkIsLogined = async() => {
+    fetch('https://port-0-bible-server-32updzt2alphmfpdy.sel5.cloudtype.app/api/users/isLogin', {
+        method: 'GET',
+        credentials : 'include',
+        headers: {
+            "Content-Type": "application/json",   
+        }
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log('로그인상태data :', data)
+    })
 }
 
 
