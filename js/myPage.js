68d99b77763a817b9e7bfb3d603f8620588a5830
seunghyun <inyo0506@gmail.com>
2024-03-12 17:03:30 +0900

    마이페이지 비밀번호 수정, 회원탈퇴기능 작업완료

--- js/myPage.js
+++ js/myPage.js
@@ -1,3 +1,4 @@
+
 // 헤더 모듈 가져오기
 function checkIsLogined() {
     {
@@ -12,7 +13,7 @@ document.addEventListener('DOMContentLoaded', checkIsLogined)
 const getUserData = async () => {
     try {
         const userEmail = localStorage.getItem('유저이름')
-        console.log('마이페이지 userEmail :',userEmail)
+        console.log('마이페이지 userEmail :', userEmail)
         const data = await fetch('http://127.0.0.1:3300/api/users/myPage', {
             method: 'POST',
             credentials: 'include',
@@ -27,8 +28,8 @@ const getUserData = async () => {
         console.log(userData)
         return userData
     }
-    catch (error){
-        console.log('myPage 조회 에러 :',error)
+    catch (error) {
+        console.log('myPage 조회 에러 :', error)
 
     }
 }
@@ -37,7 +38,7 @@ const getUserData = async () => {
 // 유저정보 DOM 그리기
 const createUserDataDom = async () => {
     const userData = await getUserData()
-    
+
     const userInfo = userData.userInfo
 
     const nameSpan = document.createElement('span')
@@ -60,14 +61,91 @@ const createUserDataDom = async () => {
     emailDiv.appendChild(emailSpan)
     mobileDiv.appendChild(mobileSpan)
 }
-
 createUserDataDom()
 
-// 비밀번호 수정하기(변경)
-
-
 // 회원탈퇴하기
+const cancleMembership = async () => {
+    const userEmail = localStorage.getItem('유저이름')
+    try {
+        const data = await fetch('http://127.0.0.1:3300/api/users/deleteUser', {
+            method: 'DELETE',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: userEmail
+            })
+        })
+        const userData = await data.json()
+        console.log('회원탈퇴 userData:', userData)
+    }
+    catch (error) {
+        console.log('회원탈퇴 에러 :', error)
+    }
+}
 
+// 비밀번호 변경시 입력란이 입력되어 있을 경우 버튼 활성화
+const transFormEditBtn = async () => {
+    const myPageSection = document.querySelector('.myPage-section')
+    myPageSection.addEventListener('input', function (e) {
+        const newPassword = document.querySelector('.newPassword').value
+        const newPasswordConfirm = document.querySelector('.newPassword-confirm').value
+        if (newPassword !== '' || newPasswordConfirm !== '') {
+            const editBtn = document.querySelector('.myPage-btn-edit')
+            editBtn.removeAttribute('disabled')
+            editBtn.style.backgroundColor = '#0ec972'
+        }
+        else if (newPassword === '' || newPasswordConfirm === '') {
+            const editBtn = document.querySelector('.myPage-btn-edit')
+            editBtn.setAttribute('disabled', true)
+            editBtn.style.backgroundColor = '#c2c2c2'
+        }
+    })
+}
+transFormEditBtn()
+
+// 마이페이지 로그아웃 
+const myPageLogout = async () => {
+    await fetch('http://127.0.0.1:3300/api/users/logout', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            "Content-Type": "application/json",
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if (!data.token) {
+                localStorage.removeItem('로그인상태')
+                localStorage.removeItem('유저이름')
+                window.location.href = 'http://127.0.0.1:5500/html/login.html'
+            }
+        })
+}
+
+// 비밀번호 변경 서버요청
+const changePw = async () => {
+    const userEmail = localStorage.getItem('유저이름')
+    const newPassword = document.querySelector('.newPassword').value
+    try {
+        const data = await fetch('http://127.0.0.1:3300/api/users/changeUserInfo', {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: userEmail,
+                password: newPassword
+            })
+        })
+        const userData = await data.json()
+        console.log('비밀번호 변경 userData:', userData)
+    }
+    catch (error) {
+        console.log('비밀번호 변경 에러 :', error)
+    }
+}
 
 // 모바일 버거버튼 클릭시
 document.body.addEventListener('click', function (e) {
@@ -76,6 +154,38 @@ document.body.addEventListener('click', function (e) {
         const mobileBackground = document.querySelector('.mobile-background')
         navButtons.classList.toggle('show')
         mobileBackground.classList.toggle('show')
+    }
+    else if (e.target.className == 'myPage-btn-edit') {
+        // 비밀번호 변경, 확인란이 입력되어 있고 일치할 경우
+        const newPassword = document.querySelector('.newPassword').value
+        const newPasswordConfirm = document.querySelector('.newPassword-confirm').value
+        if (newPassword !== newPasswordConfirm) {
+            alert('비밀번호가 일치하지 않습니다.')
+            return
+        }
+        else if (newPassword.length < 6 || newPassword.length > 12) {
+            alert('비밀번호는 6자리 이상 12자리 이하로 설정해주세요')
+            return
+        }
+        else if (newPassword === newPasswordConfirm) {
+            alert('비밀번호가 변경되었습니다. 다시 로그인해주세요.')
+            myPageLogout()
+            changePw()    
+        }
+    }
+    else if(e.target.className == 'myPage-btn-cancle'){
+        window.location.href = 'http://127.0.0.1:5500/index.html'
+    }
+    else if (e.target.className == 'myPage-btn-withdraw') {
+       // 회원탈퇴
+        const reCheck = confirm('정말로 회원탈퇴 하시겠습니까?')
+        if(reCheck){
+            alert('회원탈퇴가 완료되었습니다.')
+            myPageLogout()
+            cancleMembership()
+        } else {
+            return
+        }
     }
 })
 
